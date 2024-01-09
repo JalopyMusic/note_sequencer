015e284c1767d8ff02d1e40597fd382364a9e89e
bedwards <brian.edwards@jalopymusic.com>
2024-01-09 14:14:21 -0600

    support for not sending first note if play is pressed outside of a step start

--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,27 @@ impl Default for MyPluginParams {
 struct MyPlugin {
     params: Arc<MyPluginParams>,
     buffer_sample_rate: Option<f32>,
-    last_playing: Option<bool>,
-    last_pos_beats: Option<f64>,
+    last_playing: bool,
+    last_pos_beats: f64,
     searching_for_step: bool,
+}
+
+impl MyPlugin {
+    // send all notes off
+    const DEFAULT_LAST_PLAYING: bool = true;
+
+    // catch inital beat
+    const DEFAULT_LAST_POS_BEATS: f64 = -1.0;
+    const DEFAULT_SEARCHING_FOR_STEP: bool = true;
+
+    // used in determining if play was pressed at the start of a step
+    const STEP_THRESHOLD_DIVISOR: f64 = 32.0;
+
+    fn init(&mut self) {
+        self.last_playing = Self::DEFAULT_LAST_PLAYING;
+        self.last_pos_beats = Self::DEFAULT_LAST_POS_BEATS;
+        self.searching_for_step = Self::DEFAULT_SEARCHING_FOR_STEP;
+    }
 }
 
 impl Default for MyPlugin {
@@ -35,9 +53,9 @@ impl Default for MyPlugin {
         Self {
             params: Arc::new(MyPluginParams::default()),
             buffer_sample_rate: None,
-            last_playing: None,
-            last_pos_beats: None,
-            searching_for_step: true,
+            last_playing: Self::DEFAULT_LAST_PLAYING,
+            last_pos_beats: Self::DEFAULT_LAST_POS_BEATS,
+            searching_for_step: Self::DEFAULT_SEARCHING_FOR_STEP,
         }
     }
 }
@@ -51,17 +69,13 @@ impl Plugin for MyPlugin {
     ) -> bool {
         nih_log!("initialize");
         self.buffer_sample_rate = Some(buffer_config.sample_rate);
-        self.last_playing = None;
-        self.last_pos_beats = None;
-        self.searching_for_step = true;
+        self.init();
         true
     }
 
     fn reset(&mut self) {
         nih_log!("reset");
-        self.last_playing = None;
-        self.last_pos_beats = None;
-        self.searching_for_step = true;
+        self.init();
     }
 
     fn process(
@@ -73,7 +87,10 @@ impl Plugin for MyPlugin {
         let transport = context.transport();
 
         if !transport.playing {
-            if self.last_playing.unwrap_or(true) {
+            if self.last_playing {
+                self.last_playing = false;
+                self.last_pos_beats = Self::DEFAULT_LAST_POS_BEATS;
+                self.searching_for_step = Self::DEFAULT_SEARCHING_FOR_STEP;
                 nih_log!("all notes off: transport pause");
                 for n in 0..=127 {
                     context.send_event(NoteEvent::NoteOff {
@@ -85,8 +102,6 @@ impl Plugin for MyPlugin {
                     });
                 }
             }
-            self.last_playing = Some(false);
-            self.searching_for_step = true;
             return ProcessStatus::Normal;
         }
 
@@ -94,13 +109,6 @@ impl Plugin for MyPlugin {
             nih_log!("preroll active: do nothing");
             return ProcessStatus::Normal;
         }
-
-        if transport.playing && !self.last_playing.unwrap_or(false) {
-            // catch initial step
-            self.searching_for_step = true;
-        }
-
-        self.last_playing = Some(true);
 
         let pos_beats = match transport.pos_beats() {
             Some(value) => value,
@@ -110,35 +118,45 @@ impl Plugin for MyPlugin {
             }
         };
 
+        let tempo: f64 = match transport.tempo {
+            Some(value) => value,
+            None => {
+                nih_log!("missing tempo");
+                return ProcessStatus::Normal;
+            }
+        };
+
+        // duration of a step in fractions of a second
+        let step_seconds = 60.0 / tempo;
+
         // if a note on/off should be sent within this buffer,
-        // then timing is set to the sample index corresponding to the start of the step
+        // then timing is set to the buffer's sample index
+        //      corresponding to the start of the step
         let mut timing: Option<u32> = None;
 
-        // sometimes steps begin between buffers
-        if self.searching_for_step
-            && pos_beats.floor() > self.last_pos_beats.unwrap_or(-1.0).floor()
-        {
-            nih_log!("missed buffer containing step start, setting timing to 0");
-            timing = Some(0);
+        if self.searching_for_step && pos_beats.floor() > self.last_pos_beats.floor() {
+            if self.last_playing {
+                // sometimes steps begin between buffers
+                nih_log!("missed buffer containing step start, setting timing to 0");
+                timing = Some(0);
+            } else {
+                if pos_beats % 1.0 < step_seconds / Self::STEP_THRESHOLD_DIVISOR {
+                    // play was pressed at the start of a step
+                    nih_log!("initial step, setting timing to 0");
+                    timing = Some(0);
+                }
+            }
         }
 
-        self.last_pos_beats = Some(pos_beats);
-        self.searching_for_step = true;
+        self.last_playing = true;
+        self.last_pos_beats = pos_beats;
 
         if timing == None {
             // fraction of a beat remaining in this beat
             let remain_beats: f64 = 1.0 - pos_beats % 1.0;
 
-            let tempo: f64 = match transport.tempo {
-                Some(value) => value,
-                None => {
-                    nih_log!("missing tempo");
-                    return ProcessStatus::Normal;
-                }
-            };
-
             // fraction of a second remaining in this beat
-            let remain_seconds: f64 = remain_beats * 60.0 / tempo;
+            let remain_seconds: f64 = remain_beats * step_seconds;
 
             let buffer_samples = buffer.samples();
 
@@ -153,24 +171,25 @@ impl Plugin for MyPlugin {
             // fraction of a second this buffer represents
             let buffer_seconds: f64 = buffer_samples as f64 / buffer_sample_rate as f64;
 
-            if remain_seconds > buffer_seconds {
+            self.searching_for_step = remain_seconds > buffer_seconds;
+
+            if self.searching_for_step {
                 // buffer does not contain a beat
                 return ProcessStatus::Normal;
             }
 
             nih_log!("buffer contains start of step");
-            self.searching_for_step = false;
 
             // sample index of next beat
             let remain_samples = (buffer_sample_rate as f64 * remain_seconds).round() as i32;
 
             if remain_samples < 0 {
-                nih_log!("timing is < 0");
+                nih_log!("remain_samples is < 0");
                 return ProcessStatus::Normal;
             }
 
             if remain_samples >= buffer_samples as i32 {
-                nih_log!("timing is >= buffer size");
+                nih_log!("remain_samples is >= buffer size");
                 return ProcessStatus::Normal;
             }
 
